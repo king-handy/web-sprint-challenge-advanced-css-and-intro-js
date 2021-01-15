1c146fbe8e62f334bcee5351cb09a738810db63c
king-handy <a.king.life@pm.me>
2021-01-15 01:11:16 -0500

    artist by index

--- index.js
+++ index.js
@@ -228,12 +228,12 @@ console.log( artists[8].name = 'Vincent Van Gogh' );
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
- function getArtistByIndex(artists, index) {
-  let artist = artists[index];
-  return `the artist at index ${artist.id} is ${artist.name}`;
-}  
+ function getArtistByIndex(artists, i) {
+  let id = artists[i].id;
+  let name = artists[i].name;
 
-console.log( getArtistByIndex[2] );
+  return `the artist at index ${id} is ${name}`;
+}  
 
 
 
@@ -246,7 +246,10 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
 function get20s(artists){
-  
+  let artist = [];
+  for (let i = 0; i < artists.length; i++){
+
+  }
 }
 
 
@@ -260,8 +263,11 @@ function get20s(artists){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(artists, index){
+  let remove = [];
+  for (i = 0; i < artists.length; i++){
+    
+  }
 }
    
 
