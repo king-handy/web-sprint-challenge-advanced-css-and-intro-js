51277fba768d60daca9d7e1effb284165f7e571b
king-handy <a.king.life@pm.me>
2021-01-17 22:54:19 -0500

    stuck on get20s

--- index.js
+++ index.js
@@ -245,13 +245,10 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(artists){
-  let artist = [];
-  for (let i = 0; i < artists.length; i++){
-
-  }
+function get20s(array){
+  return array.filter( array => array.years > '1900' && array.years < '2000' );
 }
-
+console.log( get20s(artists) );
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
